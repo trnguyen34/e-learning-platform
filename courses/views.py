92ce199be7918feea06012e2cc20811c16c26b4d
Truong <trnguyen34@gmail.com>
2023-06-13 17:12:04 -0700

    added drag-and-drop functionality for the module contents

--- courses/views.py
+++ courses/views.py
@@ -184,7 +184,7 @@ class ContentOrderView(CsrfExemptMixin,
                        JsonRequestResponseMixin,
                        View):
     def post(self, request):
-        for id, order in self.request_json.item():
+        for id, order in self.request_json.items():
             Content.objects.filter(id=id,
                                    module__course__owner=request.user).update(order=order)
         return self.render_json_response({'saved': 'OK'})
