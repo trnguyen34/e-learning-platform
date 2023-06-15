6c60f4844d94681e0433b0b789d669b216d416a1
Truong <trnguyen34@gmail.com>
2023-06-14 17:43:41 -0700

    break time

--- courses/models.py
+++ courses/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
+from django.template.loader import render_to_string
+
 from .fields import OrderField
 
 class Subject(models.Model):
@@ -74,6 +76,11 @@ class ItemBase(models.Model):
     title = models.CharField(max_length=250)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    
+    def render(self):
+        return render_to_string(
+            f'courses/content/{self._meta.model_name}.html',
+            {'item':self})
     
     class Mate:
         abstract = True
